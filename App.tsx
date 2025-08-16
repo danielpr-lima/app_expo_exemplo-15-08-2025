39121675b8551876bfcb0d507fe3d6885d0d94ef
danielpr-lima <daniellima913@hotmail.com>
2025-08-16 00:45:33 +0000

    feat: implement IMC calculator and update start script in package.json

--- App.tsx
+++ App.tsx
@@ -1,20 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [peso, setPeso] = useState('');
+  const [altura, setAltura] = useState('');
+  const [imc, setImc] = useState(null);
+
+  const calcularIMC = () => {
+    const alturaMetros = altura / 100; // Convertendo de centímetros para metros
+    const imcCalculado = peso / (alturaMetros * alturaMetros);
+    setImc(imcCalculado.toFixed(2)); // Arredondando para duas casas decimais
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          value={peso}
+          onChangeText={setPeso}
+          placeholder="Peso (kg)"
+          keyboardType="numeric"
+        />
+        <TextInput
+          style={styles.input}
+          value={altura}
+          onChangeText={setAltura}
+          placeholder="Altura (cm)"
+          keyboardType="numeric"
+        />
+        <Button 
+          title="Calcular IMC" 
+          onPress={calcularIMC} 
+        />
+      </View>
+
+      {imc && (
+        <View style={styles.card}>
+          <Text style={styles.cardText}>Peso: {peso} kg</Text>
+          <Text style={styles.cardText}>Altura: {altura} cm</Text>
+          <Text style={styles.cardText}>IMC: {imc}</Text>
+        </View>
+      )}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inputContainer: {
+    width: '80%',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    padding: 10,
+    borderRadius: 100,
+  },
+  card: {
+    marginTop: 20,
+    padding: 20,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 10,
+  },
+  cardText: {
+    fontSize: 16,
+    marginBottom: 5,
   },
 });
+
+export default App;
